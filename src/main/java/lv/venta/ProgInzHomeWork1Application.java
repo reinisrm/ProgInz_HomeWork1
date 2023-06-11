e48f170625c3f9db50b3d784748cde569855c1b1
reinisrm <rreeiinniiss@gmail.com>
2023-06-11 23:11:08 +0300

    Add: Person class

---
+++ src/main/java/lv/venta/ProgInzHomeWork1Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgInzHomeWork1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgInzHomeWork1Application.class, args);
+	}
+
+}
---
+++ src/main/java/lv/venta/models/Person.java
@@ -0,0 +1,49 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "person_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Person {
+	
+	@Column(name = "Name") 
+	@NotNull
+	@Pattern(regexp = "[A-ZĀŠĒĢŪĪĶĻŅŽ]{1}[a-zēīļķšāžņģ\\ ]+")
+	@Size(min = 3, max = 20)
+	private String name;
+	
+	@Column(name = "Surname") 
+	@NotNull
+	@Pattern(regexp = "[A-ZĀŠĒĢŪĪĶĻŅŽ]{1}[a-zēīļķšāžņģ\\ ]+")
+	@Size(min = 3, max = 20)
+	private String surname;
+
+	public Person (String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
